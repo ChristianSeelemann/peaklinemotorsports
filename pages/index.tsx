78eb40400a0208c0271ace097d3d2f89c0790e1a
Christian Seelemann <cseelemann@gmx.de>
2021-11-29 23:50:45 +0100

    ✨ Next.js Basic Structure

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Peakline Motorsports</title>
+        <meta name="description" content="Simracing Team" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>Peakline Motorsports Website</main>
+    </div>
+  );
+};
+
+export default Home;
