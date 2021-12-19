fe85abd7cfad0251f27560a01e1647f0cdd01c33
Christian Seelemann <cseelemann@gmx.de>
2021-12-19 04:15:16 +0100

    ♻️ Wrap App in AuthProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Provider as AuthProvider } from "next-auth/client";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
