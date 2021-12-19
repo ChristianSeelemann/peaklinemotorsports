cbf93aa90a19bd51eb1e01a993eb015f04781dd1
Christian Seelemann <cseelemann@gmx.de>
2021-12-19 04:15:29 +0100

    ✨ Add Basic Login functionality to Index Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useSession } from "next-auth/client";
+import { signIn } from "next-auth/client";
+import { signOut } from "next-auth/client";
 
 const Home: NextPage = () => {
+  const [session, loading] = useSession();
+
+  console.log(session);
+
   return (
     <div>
       <Head>
@@ -11,6 +18,11 @@ const Home: NextPage = () => {
       </Head>
 
       <main>Peakline Motorsports Website</main>
+      <div>
+        {!session && <button onClick={() => signIn()}>Login</button>}
+        {session && session.user && `Hi ${session.user.name}`}
+      </div>
+      {session && <button onClick={() => signOut()}>Logout</button>}
     </div>
   );
 };
