b3e46cfeeddca210c245518405b524a6736d461b
Christian Seelemann <cseelemann@gmx.de>
2021-12-27 20:27:59 +0100

    ✨ Add Loading Component

---
+++ components/Loading.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Image from "next/image";
+
+export default function Loading() {
+  return (
+    <div className="fixed z-[1030] left-0 top-0 right-0 bottom-0 bg-black/50 backdrop-blur-sm grid place-items-center">
+      <div className="w-24">
+        <Image
+          src="/PeaklineLogo.svg"
+          alt="Peakline Motorsports Logo"
+          width={288}
+          height={288}
+          className="animate-pulse"
+        />
+      </div>
+    </div>
+  );
+}
