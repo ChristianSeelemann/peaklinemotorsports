4c52d64911ba70ac8b777946de1db08f227dd68b
Christian Seelemann <cseelemann@gmx.de>
2021-12-29 11:04:21 +0100

    🐛 Fix Loading Spinner Positioning

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Loading() {
   return (
-    <div className="fixed select-none z-[1030] left-0 top-0 right-0 bottom-0 bg-black/50 backdrop-blur-sm grid place-items-center">
+    <div className="fixed select-none z-[1030] left-0 top-0 right-0 bottom-0 bg-black/50 backdrop-blur-sm grid items-center justify-items-center">
       <div className="w-24">
         <Image
           src="/PeaklineLogo.svg"
