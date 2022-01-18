99fb3b8901ea3654c74097537b538e19667b21ba
Christian Seelemann <cseelemann@gmx.de>
2022-01-18 19:40:27 +0100

    🔧 Add Orange and Red to Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,8 @@ module.exports = {
       white: colors.white,
       black: colors.black,
       violet: colors.violet,
+      red: colors.red,
+      orange: colors.orange,
     },
     fontFamily: {
       montserrat: ["Montserrat", "sans-serif"],
