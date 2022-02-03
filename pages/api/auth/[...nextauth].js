7772a86adbe126fbdb13ef7910c033cddc8b712f
Christian Seelemann <cseelemann@gmx.de>
2022-02-03 12:41:43 +0100

    🔧 Remove email Login Provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,17 +12,6 @@ export default NextAuth({
       clientId: process.env.DISCORD_CLIENT_ID,
       clientSecret: process.env.DISCORD_CLIENT_SECRET,
     }),
-    EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD,
-        },
-      },
-      from: process.env.EMAIL_FROM,
-    }),
   ],
   adapter: Adapters.TypeORM.Adapter(process.env.DATABASE_URL, {
     models: {
